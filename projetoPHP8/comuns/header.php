12fa47d9820eb32c21e963454bcc9f23e449e9d4
Rene Marinho <152930400+renema@users.noreply.github.com>
2023-12-05 07:35:24 -0300

    Add files via upload
    Criando uma landing page utilizando HTML, CSS, PHP, JQUERY

---
+++ projetoPHP8/comuns/header.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content ="width=device-width, initial-scale=1">
+        <title>All In - Treinamentos</title>
+        <base href="http://localhost/">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@300;400;0,400;1,600&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" />
+        <link rel="stylesheet" type="text/css" href="projetoPHP8/recursos/css/main.css">
+        
+</head>
+<body>
+
+<header>
+<div class="container">
+<div class="conteudo">
+ <div class="logo"> 
+<img src="projetoPHP8/recursos/imagens/logo.png">
+</div>
+<div class="menu-lateral">
+<ul class="nav">
+    <li>
+        <a href="#">Quem sou eu</a>
+</li>
+<li>
+        <a href="#">Eventos</a>
+</li>
+<li>
+        <a href="#">Cursos</a>
+</li>
+<li>
+        <a href="#">Vídeos</a>
+</li>
+<li>
+        <a href="#">Fale comigo</a>
+</li>
+</ul>
+<div class="redes-sociais">
+    <p>Siga-me:</p>
+<a href=""><i class="fa-brands fa-facebook-f"></i></a>
+<a href=""><i class="fa-brands fa-instagram"></i></a>
+<a href=""><i class="fa-brands fa-youtube"></i></a>
+</div>
+</div>
+<div class="nav-bar">
+<i class="fa-solid fa-bars-staggered"></i>
+</div>
+</header>
+
---
+++ projetoPHP8/index.php
@@ -0,0 +1,10 @@
+<?php
+require_once('comuns/header.php');
+require_once('views/home.php');
+require_once('comuns/footer.php');
+
+
+
+
+
+?>
